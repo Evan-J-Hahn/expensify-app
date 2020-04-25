91adab044332921950cc481f42cc9e5fe9e50020
Evan-J-Hahn <evan.hahn@me.com>
2020-04-25 14:02:58 -0700

    Added styles

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -2,13 +2,11 @@ import React from 'react'
 import ExpenseList from './ExpenseList'
 import ExpenseListFilters from './ExpenseListFilters'
 import ExpensesSummary from './ExpensesSummary'
-import { LoginPage } from './LoginPage'
 
 const ExpenseDashboardPage = () => (
     <div>
         <ExpensesSummary />
         <ExpenseListFilters />
-        <LoginPage />
         <ExpenseList />
     </div>
 )
