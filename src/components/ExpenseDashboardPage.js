02705cbafc941314154b570a3ff48ab22c87e989
Evan-J-Hahn <evan.hahn@me.com>
2020-04-24 20:05:22 -0700

    Add private routes

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -2,11 +2,13 @@ import React from 'react'
 import ExpenseList from './ExpenseList'
 import ExpenseListFilters from './ExpenseListFilters'
 import ExpensesSummary from './ExpensesSummary'
+import { LoginPage } from './LoginPage'
 
 const ExpenseDashboardPage = () => (
     <div>
         <ExpensesSummary />
         <ExpenseListFilters />
+        <LoginPage />
         <ExpenseList />
     </div>
 )
